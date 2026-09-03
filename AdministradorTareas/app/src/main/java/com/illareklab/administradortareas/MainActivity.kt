16ee2d7d11e32dbe8f620db645435c89b72c7a22
Jhhairr2104 <jair.olvea@unmsm.edu.pe>
2026-09-03 09:45:20 -0500

    Subir proyectos Codelab 5 y Codelab 6

---
+++ AdministradorTareas/app/src/main/java/com/illareklab/administradortareas/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.illareklab.administradortareas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TaskCompletedScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskCompletedScreen(modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(R.drawable.ic_task_completed),
+            contentDescription = null
+        )
+
+        Text(
+            text = stringResource(R.string.task_completed),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(
+                top = 24.dp,
+                bottom = 8.dp
+            )
+        )
+
+        Text(
+            text = stringResource(R.string.nice_work),
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TaskCompletedPreview() {
+    MaterialTheme {
+        TaskCompletedScreen()
+    }
+}
---
+++ LearnTogether/app/src/main/java/com/illareklab/learntogether/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.illareklab.learntogether
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeArticle()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeArticle(modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = modifier
+    ) {
+
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Text(
+            text = stringResource(R.string.compose_title),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Text(
+            text = stringResource(R.string.compose_description),
+            modifier = Modifier.padding(
+                start = 16.dp,
+                end = 16.dp
+            ),
+            textAlign = TextAlign.Justify
+        )
+
+        Text(
+            text = stringResource(R.string.compose_paragraph),
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeArticlePreview() {
+    MaterialTheme {
+        ComposeArticle()
+    }
+}
